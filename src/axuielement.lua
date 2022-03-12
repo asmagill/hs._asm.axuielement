f1b2b7f5338f347e6b325bfb09077a848460fada
asmagill <asmagill@users.noreply.github.com>
2022-03-12 00:36:11 -0600

    supports building docs now

--- src/axuielement.lua
+++ src/axuielement.lua
@@ -1,6 +1,24 @@
-if not package.cpath:match(hs.configdir .. "/%?.dylib") then
-    package.cpath = hs.configdir .. "/?.dylib;" .. package.cpath
-end
+-- REMOVE IF ADDED TO CORE APPLICATION
+    repeat
+        -- add proper user dylib path if it doesn't already exist
+        if not package.cpath:match(hs.configdir .. "/%?.dylib") then
+            package.cpath = hs.configdir .. "/?.dylib;" .. package.cpath
+        end
+
+        -- load docs file if provided
+        local basePath, moduleName = debug.getinfo(1, "S").source:match("^@(.*)/([%w_]+).lua$")
+        if basePath and moduleName then
+            if moduleName == "init" then
+                moduleName = moduleName:match("/([%w_]+)$")
+            end
+
+            local docsFileName = basePath .. "/" .. moduleName .. ".docs.json"
+            if require"hs.fs".attributes(docsFileName) then
+                require"hs.doc".registerJSONFile(docsFileName)
+            end
+        end
+    until true -- executes once and hides any local variables we create
+-- END REMOVE IF ADDED TO CORE APPLICATION
 
 --- === hs.axuielement ===
 ---
@@ -47,12 +65,14 @@ if not hs.accessibilityState(true) then
     hs.luaSkinLog.ef("%s - module requires accessibility to be enabled; fix in SystemPreferences -> Privacy & Security", USERDATA_TAG)
 end
 
-local module       = require("hs.libaxuielement")
+local module       = require(table.concat({ USERDATA_TAG:match("^([%w%._]+%.)([%w_]+)$") }, "lib"))
 
-require"hs.doc".registerJSONFile(hs.processInfo["resourcePath"].."/docs.json")
-
-local log  = require("hs.logger").new(USERDATA_TAG, require"hs.settings".get(USERDATA_TAG .. ".logLevel") or "warning")
-module.log = log
+-- settings with periods in them can't be watched via KVO with hs.settings.watchKey, so
+-- in general it's a good idea not to include periods
+-- local SETTINGS_TAG = USERDATA_TAG:gsub("%.", "_")
+-- local settings     = require("hs.settings")
+-- local log          = require("hs.logger").new(USERDATA_TAG, settings.get(SETTINGS_TAG .. "_logLevel") or "warning")
+-- module.log         = log
 
 local fnutils     = require("hs.fnutils")
 local application = require("hs.application")
